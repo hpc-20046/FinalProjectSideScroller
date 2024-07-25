c3e7f717dc9e96bc5751f2c8854549ec2f075c84
hpc-20046 <20046@haurakiplains.school.nz>
2024-07-25 13:15:04 +1200

    Add project files.

---
+++ FinalProjectSideScroller.py
@@ -0,0 +1,26 @@
+import pygame
+
+WIDTH, HEIGHT = 1920, 1080
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+cell_width, cell_height = 40, 40
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                
+
+    for x in range(0, int(WIDTH/cell_width)+1):
+        for y in range(0, int(HEIGHT/cell_height)+1):
+            pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(cell_width * x, cell_height * y, cell_width, cell_height), width=1)
+
+    pygame.display.flip()
+
+pygame.quit()
