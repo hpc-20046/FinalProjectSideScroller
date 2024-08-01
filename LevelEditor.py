ac08ecdeb693e22a342586b57be28ac2530d4133
hpc-20046 <20046@haurakiplains.school.nz>
2024-08-01 15:03:20 +1200

    1/08

--- LevelEditor.py
+++ LevelEditor.py
@@ -5,22 +5,13 @@ import json
 import math
 
 
-def increment_cell_space():
-    pos = pygame.mouse.get_pos()
-    cellx = math.floor(pos[0] / cell_width)
-    celly = math.floor(pos[1] / cell_height)
-
-    if pygame.mouse.get_pressed()[2]:
-        level["level"][cellx][celly] = 0
-    else:
-        level["level"][cellx][celly] += 1
-        if level["level"][cellx][celly] > total_tiles:
-            level["level"][cellx][celly] = 0
-
-
+# pygame initialisation 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+tile_num = 0
+
+# dynamically turn all images in a directory, turn them into surfaces, and add them to a list.
 tile_surfaces = []
 tile_list = os.scandir('dungeon/')
 with tile_list as tiles:
@@ -28,6 +19,7 @@ with tile_list as tiles:
         tile_surfaces.append(pygame.transform.scale(pygame.image.load(tile.path), (cell_width, cell_height)))
 total_tiles = len(tile_surfaces)
 
+# open level file to be edited
 file = "levels/level1.json"
 
 try:
@@ -43,8 +35,10 @@ except FileNotFoundError:
     temp.pop(-1)
     level = {"level": temp}
 
+# Game loop
 running = True
 while running:
+    # Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -53,11 +47,45 @@ while running:
                 running = False
             if event.key == pygame.K_r:
                 os.system("python FinalProjectSideScroller.py")
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            increment_cell_space()
+        if pygame.mouse.get_pressed()[0]:
+            try:
+                print(event.pos)
+                pos = pygame.mouse.get_pos()
+                cellx = math.floor(pos[0] / cell_width)
+                celly = math.floor(pos[1] / cell_height)
+                level["level"][cellx][celly] = tile_num
+            except AttributeError:
+                pass
+        if pygame.mouse.get_pressed()[2]:
+            try:
+                print(event.pos)
+                pos = pygame.mouse.get_pos()
+                cellx = math.floor(pos[0] / cell_width)
+                celly = math.floor(pos[1] / cell_height)
+                level["level"][cellx][celly] = 0
+            except AttributeError:
+                pass
+        if pygame.mouse.get_pressed()[1]:
+            try:
+                print(event.pos)
+                pos = pygame.mouse.get_pos()
+                cellx = math.floor(pos[0] / cell_width)
+                celly = math.floor(pos[1] / cell_height)
+                level["level"][cellx][celly] = total_tiles
+            except AttributeError:
+                pass
+        if event.type == pygame.MOUSEWHEEL:
+            tile_num += event.y
+            pos = pygame.mouse.get_pos()
+            cellx = math.floor(pos[0] / cell_width)
+            celly = math.floor(pos[1] / cell_height)
+            level["level"][cellx][celly] = tile_num
+ 
+ 
 
     screen.fill(pygame.Color('lightblue'))
 
+    # draw grid with appropriate tiles.
     for x in range(0, int(WIDTH/cell_width)+1):
         for y in range(0, int(HEIGHT/cell_height)+1):
             if level["level"][x][y] == 0:
@@ -67,6 +95,7 @@ while running:
 
     pygame.display.flip()
 
+# write level into file
 with open(file, 'w') as outfile:
     json.dump(level, outfile)
 
