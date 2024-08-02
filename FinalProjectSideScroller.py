34d4f1601118c83851f7b3a28561157b495995c0
Pinguini456 <noahsnachos@gmail.com>
2024-08-02 18:55:39 +1200

    2/08

--- FinalProjectSideScroller.py
+++ FinalProjectSideScroller.py
@@ -1,12 +1,14 @@
 import pygame
-from settings import *
 import os
 import json
+from settings import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
 
     tile_surfaces = []
     tile_list = os.scandir('dungeon/')
@@ -24,27 +26,64 @@ def main():
 
     current_level = 1
 
+    player_obj = Player(100, HEIGHT / 2, 3)
+    player = pygame.sprite.Group()
+    player.add(player_obj)
+    player_x_change = 0
+    is_jumping = False
 
+    NEW_PLAYER_FRAME = pygame.USEREVENT
+    pygame.time.set_timer(NEW_PLAYER_FRAME, 150)
 
     running = True
     while running:
+        player_x_change = 0
+        is_jumping = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player_x_change = -5
+        if keys[pygame.K_RIGHT]:
+            player_x_change = 5
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+                if event.key == pygame.K_SPACE:
+                    is_jumping = True
+            if event.type == NEW_PLAYER_FRAME:
+                if player_x_change == 5:
+                    player.sprites()[0].update_frame("idle_right")
+                elif player_x_change == -5:
+                    player.sprites()[0].update_frame("idle_left")
+                else:
+                    player.sprites()[0].update_frame("idle_right")
+
+
 
         screen.fill((0, 0, 0))
+
+        tile_rects = []
 
         for x in range(0, int(WIDTH/cell_width)+1):
             for y in range(0, int(HEIGHT/cell_height)+1):
                 if levels[current_level - 1]["level"][x][y] == 0:
                     continue
                 else:
-                    screen.blit(tile_surfaces[levels[current_level - 1]["level"][x][y] - 1], tile_surfaces[levels[current_level - 1]["level"][x][y] - 1].get_rect(topleft=(cell_width * x, cell_height * y)))
+                    tile_rects.append(tile_surfaces[levels[current_level - 1]["level"][x][y] - 1]
+                                      .get_rect(topleft=(cell_width * x, cell_height * y)))
 
+                    screen.blit(tile_surfaces[levels[current_level - 1]["level"][x][y] - 1],
+                                tile_surfaces[levels[current_level - 1]["level"][x][y] - 1]
+                                .get_rect(topleft=(cell_width * x, cell_height * y)))
+
+        player.update(player_x_change, is_jumping, tile_rects)
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
 
 
 
--- player.py
+++ player.py
@@ -1,29 +1,96 @@
 import pygame
 from settings import *
 
+
 class Player(pygame.sprite.Sprite):
-    def __init__(self, startx, starty):
+    def __init__(self, start_x, start_y, scale_factor):
         pygame.sprite.Sprite.__init__(self)
         
         self.idle_right_frames = [
-            pygame.image.load("player/idle/right/tile000.png"),
-            pygame.image.load("player/idle/right/tile001.png"),
-            pygame.image.load("player/idle/right/tile002.png"),
-            pygame.image.load("player/idle/right/tile003.png")
+            pygame.transform.scale_by(pygame.image.load("player/idle/right/tile000.png"), scale_factor),
+            pygame.transform.scale_by(pygame.image.load("player/idle/right/tile001.png"), scale_factor),
+            pygame.transform.scale_by(pygame.image.load("player/idle/right/tile002.png"), scale_factor),
+            pygame.transform.scale_by(pygame.image.load("player/idle/right/tile003.png"), scale_factor)
             ]
         self.idle_left_frames = [
-            pygame.image.load("player/idle/left/tile004.png"),
-            pygame.image.load("player/idle/left/tile005.png"),
-            pygame.image.load("player/idle/left/tile006.png"),
-            pygame.image.load("player/idle/left/tile007.png")
+            pygame.transform.scale_by(pygame.image.load("player/idle/left/tile004.png"), scale_factor),
+            pygame.transform.scale_by(pygame.image.load("player/idle/left/tile005.png"), scale_factor),
+            pygame.transform.scale_by(pygame.image.load("player/idle/left/tile006.png"), scale_factor),
+            pygame.transform.scale_by(pygame.image.load("player/idle/left/tile007.png"), scale_factor)
             ]
-        
 
-        self.x = startx
-        self.y = starty
-        
-        self.frame = self.idle_right_frames[0]
-        self.rect = self.frame.get_rect()
-        self.rect.center = (self.x, self.y)
+        self.x = start_x
+        self.y = start_y
+        self.y_momentum = 0
+        self.jump_momentum = 10
+
+        self.state_frames = self.idle_right_frames
+        self.frame_index = 0
+        self.image = self.idle_right_frames[self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (self.x, self.y)
         
         self.state = "idle_right"
+
+    def update_frame(self, state):
+        if self.state == state:
+            self.frame_index += 1
+            if self.frame_index >= len(self.state_frames):
+                self.frame_index = 0
+            self.image = self.state_frames[self.frame_index]
+        else:
+            match state:
+                case "idle_right":
+                    self.state_frames = self.idle_right_frames
+                    self.frame_index = 0
+                    self.image = self.state_frames[self.frame_index]
+                    self.state = state
+                case "idle_left":
+                    self.state_frames = self.idle_left_frames
+                    self.frame_index = 0
+                    self.image = self.state_frames[self.frame_index]
+                    self.state = state
+                case _:
+                    self.state_frames = self.idle_right_frames
+                    self.frame_index = 0
+                    self.image = self.state_frames[self.frame_index]
+                    self.state = "idle_right"
+
+    def momentum(self, is_jumping: bool):
+        if self.y > HEIGHT - self.image.get_height():
+            self.y_momentum = -self.y_momentum
+        elif is_jumping:
+            self.y_momentum = -self.jump_momentum
+        else:
+            self.y_momentum += 0.2
+
+    def test_collisions(self, tiles):
+        collisions = []
+        for tile in tiles:
+            if self.rect.colliderect(tile):
+                collisions.append(tile)
+        return collisions
+
+    def movement(self, x_change, tiles):
+        self.x += x_change
+        self.rect.topleft = (self.x, self.y)
+        collisions = self.test_collisions(tiles)
+        for tile in collisions:
+            if x_change > 0:
+                self.rect.right = tile.left
+            if x_change < 0:
+                self.rect.left = tile.right
+        self.y += self.y_momentum
+        self.rect.topleft = (self.x, self.y)
+        collisions = self.test_collisions(tiles)
+        for tile in collisions:
+            if self.y_momentum > 0:
+                self.rect.bottom = tile.top
+                self.y_momentum = 0
+            if self.y_momentum < 0:
+                self.rect.top = tile.bottom
+                self.y_momentum = 0
+
+    def update(self, x_change, is_jumping: bool, tiles):
+        self.momentum(is_jumping)
+        self.movement(x_change, tiles)
