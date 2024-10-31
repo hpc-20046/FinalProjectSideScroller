2438f4d53994ba9ce83debafefd329f2f0ddab7e
hpc-20046 <20046@haurakiplains.school.nz>
2024-10-31 15:06:10 +1300

    blah

--- LevelEditor.py
+++ LevelEditor.py
@@ -20,7 +20,7 @@ with tile_list as tiles:
 total_tiles = len(tile_surfaces)
 
 # open level file to be edited
-file = "levels/zend_1.json"
+file = "test_levels/test.json"
 #file = "backgrounds/level1.json"
 
 try:
